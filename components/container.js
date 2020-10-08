6bb37c3f0bc169c7fd1c149ac55de332bda3e051
jcguerra46 <juancarlosguerra46@gmail.com>
2020-10-08 19:36:38 -0300

    ajustment on Headline in the article

--- components/container.js
+++ components/container.js
@@ -21,7 +21,7 @@ const Container = (props) => {
 
                             <a>Suscribite</a>
 
-                           
+
 
 
                         </div>
--- pages/[topic]/[subtopic]/[article].js
+++ pages/[topic]/[subtopic]/[article].js
@@ -6,14 +6,14 @@ const ShowArticle = (props) => {
     return (
         <Container>
             <div className="row">
-                <div className="com-titleWithfollow hlp-marginBottom-15">
+                <div className="row com-titleWithfollow hlp-marginBottom-15">
                     <h1 className="com-title-section-xl hlp-marginBottom-40">Headlines de la Receta</h1>
-                    <h2 class="com-title --threexl">Subtitle de la Receta</h2>
+                </div>
+                <div className="com-titleWithfollow hlp-marginBottom-15">
+                    <p><h2 className="com-title --threexl">Subtitle de la Receta</h2></p>
                 </div>
             </div>
-
-            
-            <img src="https://arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/24QQFAJS3ZGTFL5KXDDUNP5UJU.jpg" width="600"/>
+            <img src="https://via.placeholder.com/366x177?text=?" />
         </Container>
     )
 }
