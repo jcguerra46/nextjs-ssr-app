2d286b069b08b4a8f03c015a57cbf6ea731c5cef
jcguerra46 <juancarlosguerra46@gmail.com>
2020-10-08 19:17:19 -0300

    Header mobile ajustment

--- components/container.js
+++ components/container.js
@@ -8,6 +8,26 @@ const Container = (props) => {
     return (
         <div>
             <Header />
+
+            <header className="header-mobile">
+                <div className="lay">
+                    <div className="row">
+                        <div className="col-6">
+                            <a href="/" className="header-mobile__logo">
+                                <i className="logo-la-nacion"></i>
+                            </a>
+                        </div>
+                        <div className="col-6 hlp-text-right">
+
+                            <a>Suscribite</a>
+
+                           
+
+
+                        </div>
+                    </div>
+                </div>
+            </header>
             <main>
                 <div className="lay-sidebar">
                     
